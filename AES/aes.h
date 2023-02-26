b7c35941b60517a31be6ff4424e43d3c031758ec
Daniel Murrow <dsmurrow@gmail.com>
2023-02-26 01:39:50 -0500

    initial commit, still implementing base AES

---
+++ AES/aes.h
@@ -0,0 +1,13 @@
+#ifndef AES_H
+#define AES_H
+
+#include <inttypes.h>
+
+#define Nb 4
+#define Nk 4
+
+typedef uint8_t aes_column_t[4];
+typedef aes_column_t aes_block_t[Nb];
+
+#endif
+
---
+++ AES/operations.h
@@ -0,0 +1,9 @@
+#ifndef OPERATIONS_H
+#define OPERATIONS_H
+
+#include "aes.h"
+
+uint8_t byte_mul(uint8_t, uint8_t);
+
+#endif
+
